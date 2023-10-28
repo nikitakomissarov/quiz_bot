9c458513b8380dc4633c6c89a2af6059135ddbe4
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2023-10-28 16:19:02 +0300

    добавил регулярные выражения в проверку

--- redis_interaction.py
+++ redis_interaction.py
@@ -1,6 +1,6 @@
 import os
-
 import redis
+import re
 
 PORT = os.environ['PORT']
 HOST = os.environ['HOST']
@@ -22,8 +22,9 @@ def write_in(redis_gate, user_id, question):
 def answer_checker(quiz, redis_gate, user_id, user_answer):
     user_question = redis_gate.get(user_id).decode("utf-8")
     correct_solution = quiz.get(user_question)
-    if user_answer != correct_solution:
-        print(redis_gate.get(user_id).decode("utf-8"), quiz.get(redis_gate.get(user_id).decode("utf-8")))
+    user_answer = user_answer.split()
+    match = any(re.search(rf"\b{answer}\b", correct_solution, re.IGNORECASE) for answer in user_answer)
+    if match is False:
         result = 'Неправильно… Попробуешь ещё раз?'
     else:
         result = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
