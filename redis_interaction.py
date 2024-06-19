f1b17ff97fe634afcfbbf67f478db1585e3f2b90
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2024-06-19 14:06:19 +0300

    Update redis_interaction.py
    убрал обертки

--- redis_interaction.py
+++ redis_interaction.py
@@ -1,31 +1,24 @@
+from dotenv import load_dotenv
 import os
-import redis
 from fuzzywuzzy import fuzz
+
+load_dotenv()
 
 PORT = os.environ['PORT']
 HOST = os.environ['HOST']
 PASSWORD = os.environ['PASSWORD']
 
-
-def connection(PORT, HOST, PASSWORD):
-    connection = redis.Redis(
-        host=HOST,
-        port=PORT,
-        password=PASSWORD)
-    return connection
-
-
-def write_in(redis_gate, user_id, question):
-    redis_gate.set(user_id, question)
-
-
-def answer_checker(quiz, redis_gate, user_id, user_answer):
-    user_question = redis_gate.get(user_id).decode("utf-8")
-    correct_solution = quiz.get(user_question)
-    ration = fuzz.ratio(correct_solution.lower(), user_answer.lower())
-    coincidence_rate = 30 if len(correct_solution.split()) > 1 else 60
-    if ration < coincidence_rate:
-        result = 'Неправильно… Попробуешь ещё раз?'
+def check_answer(quiz, redis_gate, user_id, user_answer):
+    user_question = redis_gate.get(user_id)
+    if user_question is None:
+        result = 'Вопрос не найден'
+        return result
     else:
-        result = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
-    return result
+        correct_solution = quiz.get(user_question.decode('utf-8'))
+        ration = fuzz.ratio(correct_solution.lower(), user_answer.lower())
+        coincidence_rate = 30 if len(correct_solution.split()) > 1 else 60
+        if ration < coincidence_rate:
+            result = 'Неправильно… Попробуешь ещё раз?'
+        else:
+            result = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
+        return result
