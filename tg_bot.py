ce4411fa35a860db6894b50fc52efb1dc403ec98
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2024-06-19 14:05:02 +0300

    Update tg_bot.py
    поправил регистры переменных, перенес логер

--- tg_bot.py
+++ tg_bot.py
@@ -1,7 +1,9 @@
 import json
 import logging
+from dotenv import load_dotenv
 import os
 import random
+import redis
 from enum import Enum, auto
 from functools import partial
 from logging.handlers import TimedRotatingFileHandler
@@ -9,17 +11,22 @@ from logging.handlers import TimedRotatingFileHandler
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, ConversationHandler, filters
 
-from logger import TelegramLogsHandler, logger_bot
-from redis_interaction import connection, write_in, answer_checker, PORT, HOST, PASSWORD
+from logger import TelegramLogsHandler
+from redis_interaction import check_answer, PORT, HOST, PASSWORD
+
+load_dotenv()
 
 TG_TOKEN = os.environ['TG_TOKEN']
 QUIZ_FILE = os.environ['QUIZ_FILE']
+TG_CHAT_ID = os.environ['TG_CHAT_ID']
+TG_LOGGER_TOKEN = os.environ['TG_LOGGER_TOKEN']
 
+logger_bot = Application.builder().token(TG_LOGGER_TOKEN).build().bot
 
 class Handlers(Enum):
-    HANDLE_NEW_QUESTION_REQUEST = auto()
-    HANDLE_SOLUTION_ATTEMPT = auto()
-    HANDLE_GIVEUP = auto()
+    handle_new_question_request = auto()
+    handle_solution_attempt = auto()
+    handle_giveup = auto()
 
 
 logger_info = logging.getLogger('loggerinfo')
@@ -35,7 +42,7 @@ async def start(update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот для викторин.",
 
                                     reply_markup=markup)
-    return Handlers.HANDLE_NEW_QUESTION_REQUEST.value
+    return Handlers.handle_new_question_request.value
 
 
 async def cancel(update, context: ContextTypes.DEFAULT_TYPE):
@@ -48,14 +55,14 @@ async def handle_new_question_request(quiz, redis_gate, update, context: Context
     question_text = random.choice(list(quiz.keys()))
     context.user_data['correct_solution'] = quiz.get(question_text)
     await update.message.reply_text(question_text, reply_markup=markup)
-    write_in(redis_gate, user_id, question_text)
-    return Handlers.HANDLE_SOLUTION_ATTEMPT.value
+    redis_gate.set(user_id, question_text)
+    return Handlers.handle_solution_attempt.value
 
 
 async def handle_solution_attempt(quiz, redis_gate, update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.chat.id
     answer = update.message.text
-    reply = answer_checker(quiz, redis_gate, user_id, answer)
+    reply = check_answer(quiz, redis_gate, user_id, answer)
     await update.message.reply_text(reply, reply_markup=markup)
 
 
@@ -76,28 +83,31 @@ def main():
     logger_info.addHandler(handler)
     logger_error.setLevel(logging.ERROR)
     logger_error.addHandler(handler)
-    telegram_notification_handler = TelegramLogsHandler(logger_bot)
+    telegram_notification_handler = TelegramLogsHandler(logger_bot, TG_CHAT_ID)
     telegram_notification_handler.setFormatter(handler_format)
     logger_error.addHandler(telegram_notification_handler)
 
     try:
         with open(QUIZ_FILE, "r", encoding='utf-8') as quiz_file:
             quiz = json.load(quiz_file)
-
-        redis_gate = connection(PORT, HOST, PASSWORD)
+        redis_gate = redis.Redis(
+        host=HOST,
+        port=PORT,
+        password=PASSWORD)
 
         conv_handler = ConversationHandler(
+
             entry_points=[CommandHandler('start', start)],
             states={
-                Handlers.HANDLE_NEW_QUESTION_REQUEST.value: [
+                Handlers.handle_new_question_request.value: [
                     MessageHandler(
                         filters.Regex('Новый вопрос'),
                         partial(handle_new_question_request, quiz, redis_gate))
 
                 ],
-                Handlers.HANDLE_SOLUTION_ATTEMPT.value: [
+                Handlers.handle_solution_attempt.value: [
                     MessageHandler(
-                        filters.TEXT & ~filters.Regex('Новый вопрос') & ~filters.Regex('Сдаться') & ~filters.COMMAND,
+                        filters.Regex & ~filters.Regex('Новый вопрос') & ~filters.Regex('Сдаться') & ~filters.COMMAND,
                         partial(handle_solution_attempt, quiz, redis_gate)),
                     MessageHandler(
                         filters.Regex('Новый вопрос'),
