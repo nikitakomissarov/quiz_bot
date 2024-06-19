f893fd3a7fa75d4403f0d44efafd84bbb214f9ae
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2024-06-19 13:57:40 +0300

    Update logger.py
    структурировал логер

--- logger.py
+++ logger.py
@@ -1,25 +1,17 @@
 import asyncio
 import logging
-import os
-
-from telegram.ext import Application
-
-TG_CHAT_ID = os.environ['TG_CHAT_ID']
-TG_LOGGER_TOKEN = os.environ['TG_LOGGER_TOKEN']
-
-logger_bot = Application.builder().token(TG_LOGGER_TOKEN).build().bot
-
 
 class TelegramLogsHandler(logging.Handler):
 
-    def __init__(self, bot_logger):
+    def __init__(self, bot_logger, tg_chat_id):
         super().__init__()
         self.bot_logger = bot_logger
+        self.tg_chat_id = tg_chat_id
 
     def emit(self, record):
         log_entry = self.format(record)
         loop = asyncio.get_event_loop()
         if loop.is_running():
-            loop.create_task(self.bot_logger.send_message(chat_id=TG_CHAT_ID, text=log_entry))
+            loop.create_task(self.bot_logger.send_message(chat_id=self.tg_chat_id, text=log_entry))
         else:
-            loop.run_until_complete(self.bot_logger.send_message(chat_id=TG_CHAT_ID, text=log_entry))
+            loop.run_until_complete(self.bot_logger.send_message(chat_id=self.tg_chat_id, text=log_entry))
