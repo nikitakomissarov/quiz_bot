80c228ebc77381f31dfc0a62835da737350995ce
nikitakomissarov <59535117+nikitakomissarov@users.noreply.github.com>
2024-07-22 22:28:02 +0300

    изменил загрузку переменных через конфиг, спрятал логер в мейн, отфоратировал код

--- vk_bot.py
+++ vk_bot.py
@@ -1,57 +1,47 @@
 import json
 import logging
-from dotenv import load_dotenv
-import os
-import redis
 import random
 from logging.handlers import TimedRotatingFileHandler
-from telegram.ext import Application
 
+import redis
 import vk_api
+from telegram import Bot
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from config import settings
 from logger import TelegramLogsHandler
 from redis_interaction import check_answer, PORT, HOST, PASSWORD
 
-load_dotenv()
-
-VK_TOKEN = os.environ['VK_TOKEN']
-QUIZ_FILE = os.environ['QUIZ_FILE']
-TG_CHAT_ID = os.environ['TG_CHAT_ID']
-TG_LOGGER_TOKEN = os.environ['TG_LOGGER_TOKEN']
-
-logger_bot = Application.builder().token(TG_LOGGER_TOKEN).build().bot
+VK_TOKEN = settings['VK_TOKEN']
+QUIZ_FILE = settings['QUIZ_FILE']
+TG_CHAT_ID = settings['TG_CHAT_ID']
+TG_LOGGER_TOKEN = settings['TG_LOGGER_TOKEN']
 
 logger_info = logging.getLogger('loggerinfo')
 logger_error = logging.getLogger("loggererror")
-
-keyboard = VkKeyboard(one_time=True)
-keyboard.add_button('Новый вопрос', color=VkKeyboardColor.POSITIVE)
-keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
-keyboard.add_button('Мой счёт', color=VkKeyboardColor.PRIMARY)
 
 
 def handle_new_question_request(vk,
                                 quiz,
                                 redis_gate,
-                                user_id,
+                                user_id, keyboard,
                                 giveup_solution=False):
     question_text = random.choice(list(quiz.keys()))
     correct_solution = quiz.get(question_text)
     redis_gate.set(user_id, question_text)
-    reply(user_id, vk, question_text, giveup_solution)
+    reply(user_id, vk, question_text, keyboard, giveup_solution)
     return question_text, correct_solution
 
 
-def handle_solution_attempt(quiz, redis_gate, user_id, text, vk):
+def handle_solution_attempt(quiz, redis_gate, user_id, text, vk, keyboard):
     user_id = user_id
     user_answer = text
     result = check_answer(quiz, redis_gate, user_id, user_answer)
-    reply(user_id, vk, result)
+    reply(user_id, vk, result, keyboard)
 
 
-def reply(user_id, vk, text, correct_solution=False):
+def reply(user_id, vk, text, keyboard, correct_solution=False):
     vk.messages.send(user_id=user_id,
                      message=(correct_solution + '\n\n' +
                               text if correct_solution != False else text),
@@ -59,30 +49,32 @@ def reply(user_id, vk, text, correct_solution=False):
                      random_id=random.randint(1, 1000))
 
 
-def handle_vk_events(longpoll, vk, quiz, redis_gate):
+def handle_vk_events(longpoll, vk, quiz, redis_gate, keyboard):
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             text = event.text
             user_id = event.user_id
             if text == "Новый вопрос":
                 question_text, correct_solution = handle_new_question_request(
-                    vk, quiz, redis_gate, user_id)
+                    vk, quiz, redis_gate, user_id, keyboard)
             elif text == "Сдаться":
                 try:
                     giveup_solution = "Правильный ответ: " + correct_solution
                     question_text, correct_solution = handle_new_question_request(
-                        vk, quiz, redis_gate, user_id, giveup_solution)
+                        vk, quiz, redis_gate, user_id, keyboard, giveup_solution)
                 except UnboundLocalError:
                     refused_surrendering = 'Вы не можете сдаться, пока не зададите вопрос.'
-                    reply(user_id, vk, refused_surrendering)
+                    reply(user_id, vk, keyboard, refused_surrendering)
             elif text == "Привет":
                 greeting_text = "Привет! Я бот для викторин. Нажми кнопку «Новый вопрос», чтобы проверить свои знания."
-                reply(user_id, vk, greeting_text)
+                reply(user_id, vk, keyboard, greeting_text)
             else:
-                handle_solution_attempt(quiz, redis_gate, user_id, text, vk)
+                handle_solution_attempt(quiz, redis_gate, user_id, text, vk, keyboard)
 
 
 def main():
+    logger_bot = Bot(token=TG_LOGGER_TOKEN)
+
     handler = TimedRotatingFileHandler("app.log",
                                        when='D',
                                        interval=30,
@@ -98,22 +90,27 @@ def main():
     telegram_notification_handler.setFormatter(handler_format)
     logger_error.addHandler(telegram_notification_handler)
 
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button('Мой счёт', color=VkKeyboardColor.PRIMARY)
+
     while True:
         try:
             with open(QUIZ_FILE, "r", encoding='utf-8') as quiz_file:
                 quiz = json.load(quiz_file)
 
             redis_gate = redis.Redis(
-            host=HOST,
-            port=PORT,
-            password=PASSWORD)
+                host=HOST,
+                port=PORT,
+                password=PASSWORD)
 
             vk_session = vk_api.VkApi(token=VK_TOKEN)
             longpoll = VkLongPoll(vk_session)
             vk = vk_session.get_api()
 
             logger_info.info("here we go")
-            handle_vk_events(longpoll, vk, quiz, redis_gate)
+            handle_vk_events(longpoll, vk, quiz, redis_gate, keyboard)
 
         except Exception:
             logger_error.exception('Error')
